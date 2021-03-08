2715d9d9d94617cc37e18b7465d8ddd685a0f42e
esoliman <edisonsoliman26@gmail.com>
2021-03-08 06:30:11 +0000

    edited comments

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
+// on load make font 80px //
+
 window.onload = function () {
   document.querySelector("#title").style.fontSize = "80px";
 };
+
+// click to make title red //
 
 cheesedip = document.querySelector("#title");
 title.onclick = function () {
   cheesedip.classList.toggle("clicked");
 };
 
+// hidden info //
 hidden1 = document.querySelector("div#ingredients");
 hidden1.onclick = function () {
   hidden1.classList.toggle("ingredientsClicked");
@@ -21,7 +26,7 @@ hidden3 = document.querySelector("div#directions");
 hidden3.onclick = function () {
   hidden3.classList.toggle("directionsClicked");
 };
-
+// added HTML //
 x = document.getElementById("done");
 x.innerHTML = "Done and Enjoy!";
 x.style.fontSize = '40px';
